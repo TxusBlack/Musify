5ab0f05cf18332fc7a4b1d32c2f0ec403f2c6203
Diego Moreno <diegomoreno@cyandark.com>
2018-01-03 07:26:20 -0500

    Metodo para subir imagenes al server

--- controllers/user.js
+++ controllers/user.js
@@ -106,9 +106,42 @@ function updateUser(req, res) {
 	});
 }
 
+function uploadImage(req, res) {
+	var userId = req.params.id;
+	var file_name = 'Imagen no subida...';
+
+	if (req.files) {
+		// Conseguir el nombre de la imagen
+		var file_path = req.files.image.path;
+		var file_split = file_path.split('\\');
+		var file_name = file_split[2];
+
+		// Extension
+		var ext_split = file_name.split('\.');
+		var file_ext = ext_split[1];
+
+		if (file_ext == 'png' || file_ext == 'jpg' || file_ext == 'jpeg' || file_ext == 'png') {
+			User.findByIdAndUpdate(userId, { image: file_name }, (err, userUpdated) => {
+				if (!userUpdated) {
+					res.status(400).send({ message: 'No se ha podido actualizar la imagen' });
+				} else {
+					res.status(200).send({ user: userUpdated });
+				}
+			})
+		} else {
+			res.status(200).send({ message: 'Tipo de archivo no permitido' });
+		}
+
+		console.log(ext_split);
+	} else {
+		res.status(200).send({ message: 'No has subido ninguna imagen' });
+	}
+}
+
 module.exports = {
 	pruebas,
 	saveUser,
 	loginUser,
-	updateUser
+	updateUser,
+	uploadImage
 };
--- routes/user.js
+++ routes/user.js
@@ -6,10 +6,15 @@ var UserController = require('../controllers/user');
 var api = express.Router();
 var md_auth = require('../middlewares/autenticated');
 
+// Permite subir ficheros por http
+var multipart = require('connect-multiparty');
+var md_upload = multipart({ uploadDir: './uploads/users' });
+
 // Para usar un middleware se pasa como segundo parametro
 api.get('/pruebas', md_auth.ensureAuth, UserController.pruebas);
 api.post('/register', UserController.saveUser);
 api.post('/login', UserController.loginUser);
 api.put('/update-user/:id', md_auth.ensureAuth, UserController.updateUser);
+api.post('/upload-image-user/:id', [md_auth.ensureAuth, md_upload], UserController.uploadImage);
 
 module.exports = api;
